8f6b53a264dae73033abe000a2dc352e5fd0d2b2
Alexandr Dubinin <adubinin@objectstyle.com>
2019-08-02 17:06:36 +0300

    feat: add cache invalidation between builds

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, './build'),
-    filename: '[name].js',
+    filename: '[name][hash].js',
     globalObject: 'self',
   },
   module: {
