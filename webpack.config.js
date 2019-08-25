7085d65b92fa298d35675a210003e6fdcd17648d
Andrei Burak <Burak_andrej@mail.ru>
2019-08-25 17:40:33 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -75,9 +75,9 @@ module.exports = {
       template: './src/index.html'
     }),
     new CopyPlugin([
-      { from: 'src/icon.png', to: 'taskFlow/' },
-      { from: 'src/fonts', to: 'taskFlow/' },
-      { from: 'src/manifest.json', to: 'taskFlow/' },
+      { from: 'src/icon.png', to: '/' },
+      { from: 'src/fonts', to: '/' },
+      { from: 'src/manifest.json', to: '/' },
     ]),
   ],
 };
