6536ff59433bbee593b914e05886cd3a0022e6fe
Andrei Burak <andrej@timvero.com>
2019-09-14 22:33:12 +0300

    fix: header style and offline work

--- src/service-worker.js
+++ src/service-worker.js
@@ -121,7 +121,7 @@ self.addEventListener('fetch', (event) => {
     console.log('----offline----2')
     event.respondWith(new Promise(() => {}));
     db.transaction('rw', db.request, (e) => {
-      e.db.request.add(req);
+      e.db.request.add(JSON.parse(JSON.stringify(req)));
     });
     // event.waitUntil(function (){
     //   db.add({id: uuid(), req})
